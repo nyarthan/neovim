b910653c4f3ed6bb6825e18a5a7fb3c5fc124db9
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2024-09-20 23:28:44 +0200

    feat: treesitter based markdown rendering in cmp doc window

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -38,6 +38,7 @@ return {
         map("<Leader>ca", vim.lsp.buf.code_action, { mode = { "x" } })
 
         local client = vim.lsp.get_client_by_id(event.data.client_id)
+
         if
           client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight)
         then
@@ -53,7 +54,6 @@ return {
             group = highlight_augroup,
             callback = vim.lsp.buf.clear_references,
           })
-
         end
 
         if
@@ -64,21 +64,71 @@ return {
 
         if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_formatting) then
           vim.api.nvim_create_autocmd("BufWritePost", {
-            group = vim.api.nvim_create_augroup("LspAutoformat", {clear = true}),
-            callback = function()
-              vim.lsp.buf.format()
-            end
+            group = vim.api.nvim_create_augroup("LspAutoformat", { clear = true }),
+            callback = function() vim.lsp.buf.format() end,
           })
         end
 
-          vim.api.nvim_create_autocmd("LspDetach", {
-            group = vim.api.nvim_create_augroup("LspDetach", { clear = true }),
-            callback = function(event2)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = "LspHighlight", buffer = event2.buf }
-              vim.api.nvim_clear_autocmds { group = "LspAutoformat", buffer = event2.buf }
-            end,
+        --- @see [test](https://github.com/hrsh7th/nvim-cmp/issues/1699#issuecomment-1738132283)
+        local md_namespace = vim.api.nvim_create_namespace "custom/lsp-flat"
+
+        --- Adds extra inline highlights to the given buffer.
+        ---@param buf integer
+        local function add_inline_highlights(buf)
+          for l, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+            for pattern, hl_group in pairs {
+              ["@%S+"] = "@parameter",
+              ["^%s*(Parameters:)"] = "@text.title",
+              ["^%s*(Return:)"] = "@text.title",
+              ["^%s*(See also:)"] = "@text.title",
+              ["{%S-}"] = "@parameter",
+              ["|%S-|"] = "@text.reference",
+            } do
+              local from = 1 ---@type integer?
+              while from do
+                local to
+                from, to = line:find(pattern, from)
+                if from then
+                  vim.api.nvim_buf_set_extmark(buf, md_namespace, l - 1, from - 1, {
+                    end_col = to,
+                    hl_group = hl_group,
+                  })
+                end
+                from = to and to + 1 or nil
+              end
+            end
+          end
+        end
+
+        original_stylize_markdown = vim.lsp.util.stylize_markdown
+        --- HACK: Override `vim.lsp.util.stylize_markdown` to use Treesitter.
+        ---@param bufnr integer
+        ---@param contents string[]
+        ---@param opts table
+        ---@return string[]
+        ---@diagnostic disable-next-line: duplicate-set-field
+        vim.lsp.util.stylize_markdown = function(bufnr, contents, opts)
+          contents = vim.lsp.util._normalize_markdown(contents, {
+            width = vim.lsp.util._make_floating_popup_size(contents, opts),
           })
+          vim.bo[bufnr].filetype = "markdown"
+          vim.treesitter.start(bufnr)
+          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
+
+          add_inline_highlights(bufnr)
+
+          return contents
+        end
+
+        vim.api.nvim_create_autocmd("LspDetach", {
+          group = vim.api.nvim_create_augroup("LspDetach", { clear = true }),
+          callback = function(event2)
+            vim.lsp.buf.clear_references()
+            vim.api.nvim_clear_autocmds { group = "LspHighlight", buffer = event2.buf }
+            vim.api.nvim_clear_autocmds { group = "LspAutoformat", buffer = event2.buf }
+            vim.lsp.util.stylize_markdown = original_stylize_markdown
+          end,
+        })
       end,
     })
 
@@ -86,12 +136,26 @@ return {
     capabilities =
       vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
+    vim.print(capabilities)
+
     local servers = {
-      "lua_ls",
+      lua_ls = {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+          },
+          workspace = {
+            library = {
+              vim.fn.expand "~/.config/neovim",
+            },
+          },
+        },
+      },
     }
 
-    for _, server in ipairs(servers) do
+    for server, settings in pairs(servers) do
       require("lspconfig")[server].setup {
+        settings = settings,
         init_options = { documentFormatting = false },
         capabilities = capabilities,
       }
