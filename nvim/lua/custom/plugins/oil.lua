e789940f8e885e4c9b6e920c125c955e24370dc4
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2024-09-21 01:38:44 +0200

    fix(oil): always hide `nix build` result to avoid permission errors

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -8,6 +8,19 @@ return {
     delete_to_trash = true,
     skip_confirm_for_simple_edits = true,
     prompt_save_on_select_new_entry = false,
+    view_options = {
+      is_always_hidden = function(name)
+        if name ~= "result" then return false end
+
+        local stat = vim.uv.fs_lstat(name)
+        if stat and stat.type == "link" then
+          local target = vim.uv.fs_readlink(name)
+          if target and target:match "^/nix/store/" then return true end
+        end
+
+        return false
+      end,
+    },
   },
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
