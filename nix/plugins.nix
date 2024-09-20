db98782760bf720778c1593997a87053ce045622
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2024-09-21 00:49:32 +0200

    perf: link plugin content instead of copying into derivation

--- nix/plugins.nix
+++ nix/plugins.nix
@@ -12,11 +12,14 @@ in
 
     buildInputs = plugins;
 
-    installPhase = ''
-      ${lib.concatMapStringsSep "\n" (plugin: ''
-          mkdir -p $out
-          cp -r ${plugin} $out/${plugin.pname}
-        '')
-        plugins}
-    '';
+    # Why symlink plugin contents instead of dir itself?
+    # lazy.nvim does not resolve symlinks without setting dev = true
+    # because it really wants to manage plugins itself
+    # @see https://github.com/folke/lazy.nvim/issues/1063#issuecomment-1742003114
+    installPhase =
+      lib.concatMapStringsSep "\n" (plugin: ''
+        mkdir -p $out/${plugin.pname}
+        ln -s ${plugin}/* $out/${plugin.pname}/
+      '')
+      plugins;
   }
