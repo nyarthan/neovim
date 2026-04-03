5afa0d70461eea61f343919d7777692864c4d135
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2026-04-03 18:57:08 +0200

    feat: enable some builtin plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -277,3 +277,8 @@ require("conform").setup {
     lsp_format = "fallback",
   },
 }
+
+vim.cmd "packadd justify"
+vim.cmd "packadd nohlsearch"
+vim.cmd "packadd nvim.undotree"
+vim.keymap.set("n", "<lader>u", require("undotree").open)
