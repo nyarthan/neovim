8fa8c3c8382afaa0dd738a48d2d90083c3bca854
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2026-04-12 20:15:42 +0200

    feat: setup UI2 message targets

--- lua/options.lua
+++ lua/options.lua
@@ -57,4 +57,41 @@ vim.o.laststatus = 3
 vim.o.winblend = 0
 
 vim.opt.cmdheight = 0
-require("vim._core.ui2").enable()
+require("vim._core.ui2").enable {
+  enable = true,
+  msg = {
+    targets = {
+      [""] = "msg",
+      empty = "cmd",
+      bufwrite = "msg",
+      confirm = "cmd",
+      emsg = "pager",
+      echo = "msg",
+      echomsg = "msg",
+      echoerr = "pager",
+      completion = "cmd",
+      list_cmd = "pager",
+      lua_error = "pager",
+      lua_print = "msg",
+      progress = "pager",
+      rpc_error = "pager",
+      quickfix = "msg",
+      search_cmd = "cmd",
+      search_count = "cmd",
+      shell_cmd = "pager",
+      shell_err = "pager",
+      shell_out = "pager",
+      shell_ret = "msg",
+      undo = "msg",
+      verbose = "pager",
+      wildlist = "cmd",
+      wmsg = "msg",
+      typed_cmd = "cmd",
+    },
+
+    msg = {
+      height = 0.25,
+      timeout = 4000,
+    },
+  },
+}
